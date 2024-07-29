283f205cbdd80a750d2eac465bd49ab3db80c744
wychoi <soc09072@gmail.com>
2024-07-30 00:47:51 +0900

    Refactor: styles for login and main pages ing .. . 1

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,8 +2,28 @@ import styles from "./page.module.scss";
 
 export default function LoginPage() {
   return (
+    
+  <div className={styles.login_page}>
+    <h1>
+      Rainit
+    </h1>
     <div>
-        LoginPage
+      <div className={styles.auth_title}>
+          로그인
+      </div>
+      <div>
+          <input type="text" placeholder="아이디"/>
+      </div>
+      <div>
+          <input type="password" placeholder="비밀번호"/>
+      </div>
+      
     </div>
+    <div className={styles.login_button}>
+      
+      로그인
+    </div>
+  </div>
+
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,25 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <div>
+        <div className={styles.logo}>
+          <div>
+            <div>daily</div>
+            <div>Pet</div>
+          </div>
+        </div>
+        <div>
+        
+        </div>
+      </div>
+      <div>
+      <div>
+        
+        </div>
+        <div>
+        
+        </div>
+      </div>
     </main>
   );
 }
