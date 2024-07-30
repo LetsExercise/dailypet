2fe5ad4af5b494bf544b88c299c6c7a1fc0b8c29
dandamdandam <glue0440@gmail.com>
2024-07-30 21:57:03 +0900

    feat: add Logo component

---
+++ src/app/_common/Logo/index.tsx
@@ -0,0 +1,10 @@
+import style from "./index.module.scss";
+
+export default function Logo() {
+  return (
+    <div className={style.logo}>
+      <div>daily</div>
+      <div>Pet</div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
+import Logo from "./_common/Logo";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div>
         <div className={styles.logo}>
-          <div>
-            <div>daily</div>
-            <div>Pet</div>
-          </div>
+          <Logo />
         </div>
-        <div>
-        
-        </div>
+        <div></div>
       </div>
       <div>
-      <div>
-        
-        </div>
-        <div>
-        
-        </div>
+        <div></div>
+        <div></div>
       </div>
     </main>
   );
