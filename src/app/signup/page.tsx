cf05536f9980c454b6c81acd6ffdf98ec2bc82b9
dandamdandam <glue0440@gmail.com>
2024-07-31 21:57:10 +0900

    design: 회원가입 페이지 UI

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,21 +1,32 @@
 "use client";
 
 import { FormEvent } from "react";
+import { useRouter } from "next/navigation";
 import styles from "./page.module.scss";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "../../lib/firebase";
 import { doc, setDoc } from "firebase/firestore";
+import Logo from "../_common/Logo";
 
-export default function LoginPage() {
+// TODO: validation 작업. (react-hook-form 라이브러리 추천)
+export default function SignUpPage() {
+  const router = useRouter();
+
   const handleSignup = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const data = Object.fromEntries(new FormData(e.currentTarget));
+    if (data.password !== data.check_password) {
+      alert("비밀번호가 일치하지 않습니다. 다시 시도해주세요.");
+      return;
+    }
+
     createUserWithEmailAndPassword(auth, String(data.email), String(data.type))
       .then((userCredential) => {
         setDoc(doc(db, "users", userCredential.user.uid), {
           userId: String(data.name),
         });
+        router.push("/login");
       })
       .catch((error) => {
         // TODO: 이메일 형식이 잘못되거나 중복될 경우 사용자 관점에서 명확한 에러 처리
@@ -24,22 +35,34 @@ export default function LoginPage() {
       });
   };
   return (
-    <div className={styles.login_page}>
-      <h1>Rainit</h1>
-      <form onSubmit={(e) => handleSignup(e)}>
-        <div className={styles.auth_title}>회원가입</div>
-        <input name="name" placeholder="닉네임" required />
-        <input type="email" name="email" placeholder="이메일" required />
-        <input
-          type="password"
-          name="password"
-          placeholder="비밀번호"
-          required
-        />
-        <button className={styles.login_button} type="submit">
-          회원가입
-        </button>
-      </form>
-    </div>
+    <form onSubmit={(e) => handleSignup(e)} className={styles.main}>
+      <div />
+      <div>
+        <Logo />
+        <div className={styles.inputs}>
+          <input name="name" placeholder="닉네임을 입력하세요" required />
+          <input
+            type="email"
+            name="email"
+            placeholder="이메일을 입력하세요"
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="비밀번호를 입력하세요"
+            required
+          />
+          <input
+            type="password"
+            name="check_password"
+            placeholder="비밀번호를 다시 입력하세요"
+          />
+        </div>
+      </div>
+      <button className={styles.button} type="submit">
+        회원가입
+      </button>
+    </form>
   );
 }
