e0d94ea9bb48944eb9e5adebac0c8a3c3e936332
Mule129 <mule01129@gmail.com>
2024-08-04 16:56:45 +0900

    [Feat] mainView

---
+++ src/app/home/homeView.tsx
@@ -0,0 +1,21 @@
+import Floor from './floor.svg'
+import Character from './character.svg'
+
+import styles from "./page.module.scss";
+
+import Image from 'next/image'
+
+
+
+export default function HomeView() {
+    return (
+        <div className={styles.character}>
+            <div className={styles.forward}>
+                <Image src={Character} alt='character'></Image>
+            </div>
+            <div className={styles.background}>
+                <Image src={Floor} alt='floor'></Image>
+            </div>
+        </div>
+    )
+}
