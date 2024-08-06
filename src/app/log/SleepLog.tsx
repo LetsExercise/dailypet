b7af2d315c2ca800c154930794ef0b62135d6914
Mule129 <mule01129@gmail.com>
2024-08-06 20:33:40 +0900

    [Feat] #21 - add css and today score

--- src/app/log/SleepLog.tsx
+++ src/app/log/SleepLog.tsx
@@ -2,22 +2,34 @@
 
 import { useSleepOfDay } from "@/lib/firebase/api/sleepAPI";
 
+import styles from './page.module.scss'
+import { logInput } from "./page";
+
+
 export default function SleepLog() {
   const { data, isLoading, isError } = useSleepOfDay(new Date());
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error</div>;
-  if (!data) return <div></div>;
+  if (!data) return <div>활동을 추가해보세요!</div>;
 
   return (
-    <div>
-      <h2>수면</h2>
-      {data.map((sleep) => (
-        <div key={sleep.id}>
-          <div>{sleep.label}</div>
-          <div>{sleep.sleepStart.toISOString()}</div>
-          <div>{sleep.sleepEnd.toISOString()}</div>
-        </div>
-      ))}
+    <div className={styles.add_log_frame}>
+      <div className={styles.add_log_title}>
+        <div>수면습관</div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" onClick={logInput}>
+          <path d="M12 6L12 18" stroke="#535353" stroke-width="2" stroke-linecap="round"/>
+          <path d="M18 12L6 12" stroke="#535353" stroke-width="2" stroke-linecap="round"/>
+        </svg>
+      </div>
+      <div>
+        {data.map((sleep) => (
+          <div key={sleep.id} className={styles.log_input}>
+            <div>{sleep.label}</div>
+            <div>{sleep.sleepStart.toISOString()}</div>
+            <div>{sleep.sleepEnd.toISOString()}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/app/log/WorkoutLog.tsx
+++ src/app/log/WorkoutLog.tsx
@@ -2,22 +2,34 @@
 
 import { useWorkoutOfDay } from "@/lib/firebase/api/workoutAPI";
 
+import styles from './page.module.scss'
+import { logInput } from "./page";
+
 export default function WorkoutLog() {
   const { data, isLoading, isError } = useWorkoutOfDay(new Date());
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error</div>;
-  if (!data) return <div></div>;
+  if (!data) return <div>활동을 추가해보세요!</div>;
 
   return (
-    <div>
-      <h2>운동</h2>
-      {data.map((workout) => (
-        <div key={workout.id}>
-          <div>{workout.exerciseName}</div>
-          <div>{workout.duration}</div>
-          <div>{workout.datetime.toISOString()}</div>
-        </div>
-      ))}
+    <div className={styles.add_log_frame}>
+      <div className={styles.add_log_title}>
+        <div>운동</div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" onClick={logInput}>
+          <path d="M12 6L12 18" stroke="#535353" stroke-width="2" stroke-linecap="round"/>
+          <path d="M18 12L6 12" stroke="#535353" stroke-width="2" stroke-linecap="round"/>
+        </svg>
+      </div>
+      <div>
+        {data.map((workout) => (
+          <div key={workout.id} className={styles.log_input}>
+            <div>{workout.exerciseName}</div>
+            <div>{workout.duration}</div>
+            <div>{workout.datetime.toISOString()}</div>
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 }
